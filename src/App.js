19eb38e2197b7d51a89e08d581f5dae4137f41b5
Rich Arce <raarce118@gmail.com>
2022-05-16 17:24:21 -0700

    grid

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div className = "container">
+  <div className="logo">Logo</div>
+  <div className="header">header</div>
+  <div className="nav">nav</div>
+  <div className="content">content</div>
+  <div className="footer">footer</div>
+
+</div>
   );
 }
 
